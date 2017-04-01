0729d75188c3d07d50c67c6756aee167a91d4690
122006 <1109108466@qq.com>
2017-04-01 10:50:00 +0800

    TimeUtils

---
+++ by122006library/src/main/java/com/by122006library/Utils/TimeUtils.java
@@ -0,0 +1,49 @@
+package com.by122006library.Utils;
+
+/**
+ * Created by admin on 2017/4/1.
+ */
+
+public class TimeUtils {
+    public static String getStandardDate(long t){
+
+        StringBuffer sb = new StringBuffer();
+
+        long time = System.currentTimeMillis();
+        long mill = (long) Math.ceil(time /1000);//秒前
+
+        long minute = (long) Math.ceil(time/60/1000.0f);// 分钟前
+
+        long hour = (long) Math.ceil(time/60/60/1000.0f);// 小时
+
+        long day = (long) Math.ceil(time/24/60/60/1000.0f);// 天前
+
+        if (day - 1 > 0) {
+            sb.append(day + "天");
+        } else if (hour - 1 > 0) {
+            if (hour >= 24) {
+                sb.append("1天");
+            } else {
+                sb.append(hour-1 + "小时");
+            }
+        } else if (minute - 1 > 0) {
+            if (minute == 60) {
+                sb.append("1小时");
+            } else {
+                sb.append(minute-1 + "分钟");
+            }
+        } else if (mill - 1 > 0) {
+            if (mill == 60) {
+                sb.append("1分钟");
+            } else {
+                sb.append(mill-1 + "秒");
+            }
+        } else {
+            sb.append("刚刚");
+        }
+        if (!sb.toString().equals("刚刚")) {
+            sb.append("前");
+        }
+        return sb.toString();
+    }
+}
